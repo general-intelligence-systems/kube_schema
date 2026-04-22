0cfcdde56d78d3575c197fdced18fe6a275bff53
Nathan Kidd <nathankidd@hey.com>
2026-04-22 01:07:05 +0100

    Quick Git Push

--- flake.nix
+++ flake.nix
@@ -10,10 +10,6 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         ruby = pkgs.ruby_3_4; # Specify version
-        kubectlWithKubeconfig = pkgs.writeShellScriptBin "kubectl" ''
-          #!${pkgs.bash}/bin/bash
-          KUBECONFIG="$PWD/kubeconfig.yaml" ${pkgs.kubectl}/bin/kubectl "$@"
-        '';
       in
       {
         devShells.default = pkgs.mkShell {
@@ -25,7 +21,6 @@
             ruby
             pkgs.libyaml # psych gem
             pkgs.openssl # openssl gem
-            kubectlWithKubeconfig
           ];
 
           shellHook = ''
@@ -34,7 +29,6 @@
             export PATH="$GEM_HOME/bin:$PATH"
             export BUNDLE_PATH="$GEM_HOME"
             export BUNDLE_BIN="$GEM_HOME/bin"
-            export KUBECONFIG="$PWD/kubeconfig.yaml"
           '';
         };
       }
