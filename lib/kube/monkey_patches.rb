65cc7abea445b13e7437a4ff2fe2ebf118791f8e
Nathan Kidd <nathankidd@hey.com>
2026-04-23 15:15:46 +0100

    added #vivify

--- lib/kube/monkey_patches.rb
+++ lib/kube/monkey_patches.rb
@@ -20,4 +20,17 @@ class Hash
   #def respond_to_missing(name, _priv = false)
   #  name.to_s.end_with?("=") || key?(name.to_sym)
   #end
+
+  # Build a Hash with autovivification via a block DSL.
+  #
+  #   Hash.vivify {
+  #     metadata.name = "web"
+  #     spec.replicas = 3
+  #     spec.selector.matchLabels.app = "web"
+  #   }
+  #   # => { metadata: { name: "web" }, spec: { replicas: 3, selector: { matchLabels: { app: "web" } } } }
+  #
+  def self.vivify(&block)
+    new.tap { |h| h.instance_exec(&block) }
+  end
 end
--- lib/kube/schema/version.rb
+++ lib/kube/schema/version.rb
@@ -2,6 +2,6 @@
 
 module Kube
   module Schema
-    VERSION = "1.3.5"
+    VERSION = "1.3.6"
   end
 end
